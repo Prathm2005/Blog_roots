0afd13c0cc2b0282bdf007d3eede7d2c6cbd80ac
Prathm2005 <141560484+Prathm2005@users.noreply.github.com>
2024-11-05 00:00:52 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-   
+   images: {
+        domains: ['your-api-domain.com'], // Add the domain where your images are hosted
+    },
   };
   
   export default nextConfig;
