c901aa2da787b2b2314bd9b2378e8f233d284c81
Prathmesh <prathmesh.malunjkar02@gmail.com>
2024-11-04 18:55:18 +0530

    Resolve merge conflicts

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-<<<<<<< HEAD
-    unoptimized: true,
-=======
-    domains: ['blog-roots.onrender.com'], // Your deployed domain
->>>>>>> 6b25caae0b36c54d98d056b509d1366d48d83fd9
-  },
-};
-
-export default nextConfig;
+    images: {
+      unoptimized: true,
+    },
+  };
+  
+  export default nextConfig;
+  
