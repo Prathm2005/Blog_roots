9c64859e9e0918e4720dd1b9ba77e53dbe721416
Prathm2005 <141560484+Prathm2005@users.noreply.github.com>
2024-11-07 00:37:14 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-   images: {
-        domains: ['blogroots.onrender.com'], // Add the domain where your images are hosted
-    },
-  };
-  
-  export default nextConfig;
-  
+  trailingSlash: false, // Add trailingSlash configuration
+};
+
+module.exports = nextConfig;
