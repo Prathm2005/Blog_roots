115f9ad0e4fb99765445afdb26fcf08c4f21ccf7
Prathm2005 <141560484+Prathm2005@users.noreply.github.com>
2024-11-07 11:32:53 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   trailingSlash: true, // Your setting
+   images: {
+    unoptimized: true, // Disables Next.js image optimization
+  },
 };
 
 export default nextConfig;
